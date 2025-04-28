425d323220c517f37c3ec5a799896ef728686554
Tamara <tm.vanzundert@student.avans.nl>
2025-04-28 18:25:17 +0200

    Set up the repo with a skeleton website

---
+++ website-components/header.php
@@ -0,0 +1,21 @@
+<header>
+    <!-- Display the logo and the navbar at the top of the website -->
+    <img class="header-logo" src="./images/header/logo.png" alt="Logo Plug & Play">
+    <nav class="header-links">
+        <ul>
+            <?php 
+            // Define navigation links
+            $navLinks = [
+                'Home' => 'index.php',
+                'Over Ons' => 'over-ons.php',
+                'Producten en Diensten' => 'producten.php',
+                'Contact' => 'contact.php'
+            ];
+
+            // Render navigation links
+            foreach ($navLinks as $label => $url): ?>
+                <li><a href="<?= $url ?>" aria-label="<?= strtolower($label) ?>"><?= $label ?></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+</header>
