c4fbf7809f0bdbe3bef8fe0b501f27c29a66e738
tmvanzundert <tm.vanzundert@student.avans.nl>
2025-05-04 10:22:49 +0200

    Add language files for nl and en

---
+++ languages/nl.php
@@ -0,0 +1,17 @@
+<?php
+$translations = [
+    // Navigation
+    'nav_home' => 'Home',
+    'nav_about' => 'Over Ons',
+    'nav_products' => 'Producten en Diensten',
+    'nav_contact' => 'Contact',
+    
+    // Common elements
+    'copyright' => 'Alle rechten voorbehouden',
+    
+    // Home page
+    'welcome' => 'Welkom bij Plug & Play',
+    
+    // Add more translations as needed
+];
+?>
