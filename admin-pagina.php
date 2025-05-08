575f8c3b5e358dc9003ac764ec2a6c6e77754f41
Stephanvdriel <sp.vandriel@student.avans.nl>
2025-05-08 19:22:22 +0200

    Styling and settings for csvimport

--- admin-pagina.php
+++ admin-pagina.php
@@ -12,10 +12,10 @@
     <div Class="csv-upload">
         <h2>Import CSV File</h2>
         <p>Import CSV file to save data in the database.</p>
-        <!-- <form action="" method="post" name="frmCSVImport" id="frmCSVImport"
+        <form action="" method="post" name="frmCSVImport" id="frmCSVImport"
             enctype="multipart/form-data" onsubmit="return validateFile()">
             <div Class="input-row">
-                <label>Choose your file. <a href="./import-template.csv" download>Download
+                <label>Choose your file. <a href="website-components/import-template.csv" download>Download
                         template</a></label> <input type="file" name="file" id="file"
                     class="file" accept=".csv,.xls,.xlsx">
                 <div class="import">
@@ -23,7 +23,7 @@
                         CSV and Save Data</button>
                 </div>
             </div>
-        </form> -->
+        </form>
     </div>
 
 
