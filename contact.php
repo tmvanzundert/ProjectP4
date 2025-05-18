982323c5a38a1fc57a074a4c8370caa9401b7e2d
Stephanvdriel <sp.vandriel@student.avans.nl>
2025-05-18 19:46:31 +0200

    Adding CSS to contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,19 @@
 <?php
 
-    require_once 'website-components/handlers.php';
-    require_once 'scripts/php/info.php';
-    $contactFieldsValid = validateContactInfoFields();
-    if ($contactFieldsValid === true) {
+require_once 'website-components/handlers.php';
+require_once 'scripts/php/info.php';
+$contactFieldsValid = validateContactInfoFields();
+if ($contactFieldsValid === true) {
 
-        $_SESSION['sendSuccessfully'] = true;
+    $_SESSION['sendSuccessfully'] = true;
 
-        // Empty POST array
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            header("Location: " . $_SERVER['PHP_SELF']);
-            exit;
-        }
-
+    // Empty POST array
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
     }
+
+}
 
 ?>
 
@@ -26,27 +26,79 @@
 
     <?php include 'website-components/header.php'; ?>
 
-    <form action="" method="post" class="contact-form">
 
-        <input type="text" placeholder="Voornaam" name="firstname" value="<?php echo isset($_POST['firstname']) ? $_POST['firstname'] : ''; ?>" >
-        <input type="text" placeholder="Achternaam" name="lastname" value="<?php echo isset($_POST['lastname']) ? $_POST['lastname'] : ''; ?>" >
-        <input type="email" placeholder="E-mail" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" >
-        <input type="text" placeholder="Telefoonnummer" name="phonenumber" value="<?php echo isset($_POST['phonenumber']) ? $_POST['phonenumber'] : ''; ?>" >
-        <input type="text" placeholder="Onderwerp" name="subject" value="<?php echo isset($_POST['subject']) ? $_POST['subject'] : ''; ?>" >
-        <textarea placeholder="Bericht" name="message" ><?php echo isset($_POST['message']) ? $_POST['message'] : ''; ?></textarea>
-        <input type="submit" name="submit" class="submit-button">
-        
-    </form>
-    <?php if ($contactFieldsValid !== true && $contactFieldsValid !== false) : ?>
-        <div class="error-message">
-            <p><?php echo $contactFieldsValid; ?></p>
+    <section class="contact-banner">
+        <div>
+            <h1>Contact</h1>
         </div>
-    <?php elseif (isset($_SESSION['sendSuccessfully']) && $_SESSION['sendSuccessfully']) : ?>
-        <div class="success-message">
-            <p>Je bericht is succesvol verzonden!</p>
+    </section>
+
+    <section class="contact-info">
+        <form class="info-form" action="" method="post" class="contact-form">
+
+            <input type="text" placeholder="Voornaam" name="firstname"
+                value="<?php echo isset($_POST['firstname']) ? $_POST['firstname'] : ''; ?>">
+            <input type="text" placeholder="Achternaam" name="lastname"
+                value="<?php echo isset($_POST['lastname']) ? $_POST['lastname'] : ''; ?>">
+            <input type="email" placeholder="E-mail" name="email"
+                value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>">
+            <input type="text" placeholder="Telefoonnummer" name="phonenumber"
+                value="<?php echo isset($_POST['phonenumber']) ? $_POST['phonenumber'] : ''; ?>">
+            <input type="text" placeholder="Onderwerp" name="subject"
+                value="<?php echo isset($_POST['subject']) ? $_POST['subject'] : ''; ?>">
+            <textarea placeholder="Bericht"
+                name="message"><?php echo isset($_POST['message']) ? $_POST['message'] : ''; ?></textarea>
+            <button type="submit">Verstuur</button>
+            <!-- <input type="submit" name="submit" class="submit-button"> -->
+            <p>
+                <?php if ($contactFieldsValid !== true && $contactFieldsValid !== false): ?>
+                    <div class="error-message">
+                        <p><?php echo $contactFieldsValid; ?></p>
+                    </div>
+                <?php elseif (isset($_SESSION['sendSuccessfully']) && $_SESSION['sendSuccessfully']): ?>
+                    <div class="success-message">
+                        <p>Je bericht is succesvol verzonden!</p>
+                    </div>
+                    <?php unset($_SESSION['sendSuccessfully']); ?>
+                <?php endif; ?>
+            </p>
+        </form>
+
+            
+
+        <div class="info-text">
+
+            <h2>DE KOFFIE STAAT KLAAR!</h2>
+            <p>
+                Bent u nieuwsgierig geworden naar de mogelijkheden om samen met ons te werken? Neem dan contact met ons
+                op.
+                In een persoonlijk gesprek vertellen wij u graag meer.
+                U vindt ons in het Brabantse Breda.
+                Wij zorgen dat de koffie klaarstaat!
+            </p>
+            <h3>U kunt ons bereiken op:</h3>
+            <p>Plug & Play
+            <p>
+            <p>Hogeschoollaan 1</p>
+            <p>4818 CR Breda</p>
+
+            <P>Telefoon: <a href="tel:0031850073030">088 525 7500</a></p>
+            <p>E-mail: <a href="mailto:info@plugplay.pro">info@plugplay.pro</a></p>
+
         </div>
-        <?php unset($_SESSION['sendSuccessfully']); ?>
-    <?php endif; ?>
+
+        <div class="info-map">
+            <h3>Bekijk ons op de kaart</h3>
+
+            <iframe width="30%" height="60%"
+                src="https://www.openstreetmap.org/export/embed.html?bbox=4.795634150505067%2C51.5823632693168%2C4.799271225929261%2C51.58643982276231&amp;layer=mapnik&amp;marker=51.58440159175703%2C4.797452688217163"
+                style="border: 1px solid black">
+            </iframe>
+        </div>
+
+    </section>
+
+    
 
     <?php include 'website-components/footer.php'; ?>
 
